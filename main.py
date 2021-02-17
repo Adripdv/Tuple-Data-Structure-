6d2d87830c6c2bcc2cd9a0853b9257c9d3019cf3
Adriana Oliveira <adripdv@gmail.com>
2021-02-17 15:35:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# Tuple items are enclosed in round brackets 
+# Tuple is ordered 
+# Tuple is immutable (different from lists that are mutable)
+# Tuple elements do no need to be unique 
+# Elements can be of different data Type 
+
+''' Creating Tuple '''
+
+
+''' Access Tuple Elements '''
+
+
+''' Negative indexing '''
+
+''' Slicing Tuples in Python '''
+
+
+''' Changing a Tuple '''
+
+''' Deleting a Tuple '''
+
+''' Tuple Methods '''
