aa2cac39d5ca7864a1b88a99c4a95419e8dadafe
Adriana Oliveira <adripdv@gmail.com>
2021-02-17 16:03:30 +0000

    completed lesson

--- main.py
+++ main.py
@@ -6,17 +6,47 @@
 
 ''' Creating Tuple '''
 
+# tuple = ()
+# tuple = ('cat', [4,4,2,1,6], (1,2,3))
+# tuple = 1234, 5678
+# tuple = 'hello' #Not a tuple just a string
+# tuple = 'hello',
+# tuple = ('hello') # Also not a tuple just a string 
+# tuple = ('hello',)
+# print(tuple)
 
 ''' Access Tuple Elements '''
 
-
-''' Negative indexing '''
+# tuple = (12345, 4321, 'hello', [7,8,9], (0,9,8,7))
+# print(tuple[0])
+# print(tuple[-2])
 
 ''' Slicing Tuples in Python '''
+
+# tuple = (('orange', 'banana'), 12345, [5,6,7], ('carrot', 'kale'))
+# # print(tuple[:])
+# # print(tuple[2:])
+# # print(tuple[2:5])
+# print(tuple[::2])
 
 
 ''' Changing a Tuple '''
 
+# fruits = ('apple', 'banana', 'orange', 'lemon', 'grapes')
+# fruits [0] = 'pear' # TypeError: 'tuple' object does not support item assignment
+
 ''' Deleting a Tuple '''
 
+# fruits = ('apple', 'banana', 'orange', 'lemon', 'grapes')
+# del fruits[0] # TypeError: 'tuple' object doesn't support item deletion
+# print(fruits)
+
+# del fruits
+# print(fruits) # NameError: name 'fruits' is not defined (deleted successfuly)
+
 ''' Tuple Methods '''
+# print(dir(tuple)) # only two methods (count and index)
+
+# fruits = ('apple', 'banana', 'orange', 'lemon', 'grapes')
+# print(fruits.count('orange'))
+# print(fruits.index('lemon'))
